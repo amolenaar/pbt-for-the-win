ed42784f69b55acea7c91f5f47d53b9e5f891489
Arjan Molenaar <gaphor@gmail.com>
2022-02-18 10:34:22 +0100

    Add simple unit tested version of simplified sqrt

--- tests/test_pbt_for_the_win.py
+++ tests/test_pbt_for_the_win.py
@@ -1,5 +1,27 @@
+import pytest
+import hypothesis
+
 from pbt_for_the_win import __version__
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def simplify_sqrt(radicand):
+    if radicand < 1:
+        return None
+    for i in range(radicand // 2, 1, -1):
+        if radicand % (i*i) == 0:
+            return i, radicand // (i*i)
+    return (1, radicand)
+
+@pytest.mark.parametrize(
+    "radicand,outcome",
+    [
+        [2, (1, 2)],
+        [4, (2, 1)],
+        [9, (3, 1)],
+        [12, (2, 3)],
+    ]
+)
+def test_simplify_sqrt(radicand, outcome):
+    
+    assert simplify_sqrt(radicand) == outcome
+
